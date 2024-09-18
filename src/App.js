3585edaf5539dda8e50520d996eab9b148d7a18d
ZezetuSibara <123359014+ZezetuSibara@users.noreply.github.com>
2024-09-19 01:41:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "fa506aa8bdtd3d64a554291oa74a69fb";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
 }
