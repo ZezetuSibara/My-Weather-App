cf79ec39c6918a044d16d9a0b919485e606fe0cb
ZezetuSibara <123359014+ZezetuSibara@users.noreply.github.com>
2024-09-19 01:02:18 +0200

    Added/imported Axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+// Importing axios (make sure axios is included in your project)
+import axios from "axios";
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "fa506aa8bdtd3d64a554291oa74a69fb";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
