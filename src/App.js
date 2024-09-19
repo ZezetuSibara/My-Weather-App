c5a3bd25ef54a772cc5f58ac405bd6c125d0fcaf
ZezetuSibara <123359014+ZezetuSibara@users.noreply.github.com>
2024-09-19 23:38:27 +0200

    Updated infor for search Engine API

--- src/App.js
+++ src/App.js
@@ -11,18 +11,19 @@ function refreshWeather(response) {
 
 function searchCity(city) {
     let apiKey = "fa506aa8bdtd3d64a554291oa74a69fb";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Cape Town");
+searchCity("Johannesburg");
